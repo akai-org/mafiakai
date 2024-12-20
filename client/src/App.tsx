2a82163c608a20569387377f655e4ddd4b9a6a70
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2024-12-20 21:32:42 +0100

    feat: simple setup of router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { About, Home } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center gap-y-4">
-      <h1 className="text-4xl">Main view</h1>
-
-      <button
-        className="px-4 py-2 border-2 border-blue-600 rounded-3xl transition-transform hover:scale-110 outline-none active:scale-100"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is {count}
-      </button>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;

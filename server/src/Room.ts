e26725cd2b516eef1213e0a987a5561d72a7371b
Franciszek Kaczmarek <franek.kaczmarek45gmail.com>
2024-12-23 12:27:01 +0100

    split player, room, manager to diferrent files

---
+++ server/src/Room.ts
@@ -0,0 +1,18 @@
+import { Player } from "./Player";
+
+export class Room {
+  code: string;
+  players: Map<string, Player> = new Map();
+
+  constructor(code: string) {
+    this.code = code;
+  }
+
+  addPlayer(player: Player) {
+    this.players.set(player.id, player);
+  }
+
+  removePlayer(playerId: string) {
+    this.players.delete(playerId);
+  }
+}

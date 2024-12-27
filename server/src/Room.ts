2b5159f06c6c7c787287a4bdab0d0bd37d6c7133
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-12-27 15:12:29 +0100

    feat: add current phase Room attribute

--- server/src/Room.ts
+++ server/src/Room.ts
@@ -1,8 +1,10 @@
+import { Phases } from "@global/Game";
 import { Player } from "./Player";
 
 export class Room {
   code: string;
   players: Map<string, Player> = new Map();
+  phase: Phases = Phases.LOBBY;
 
   constructor(code: string) {
     this.code = code;

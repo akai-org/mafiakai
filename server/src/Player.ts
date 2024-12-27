60dfc658a6a6739864c3a0d07f354ec5d55091a2
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-12-27 14:55:40 +0100

    feat: lock Player role attribute to enum values

--- server/src/Player.ts
+++ server/src/Player.ts
@@ -1,7 +1,9 @@
+import { Roles } from "@global/Roles";
+
 export class Player {
   constructor(
     public name: string,
     public id: string,
-    public role: string
+    public role: Roles 
   ) {}
 }

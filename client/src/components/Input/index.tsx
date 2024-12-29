0ed9437324ca7f71c2e1ed8080d4661bf7093296
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2024-12-29 18:24:12 +0100

    feat(Input): forward ref

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ ...props }: InputProps) {
-  return (
-    <input
-      {...props}
-      className={
-        "rounded-md border-2 border-black px-2 py-2 text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-black"
-      }
-    />
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(({ ...props }, ref) => (
+  <input
+    {...props}
+    ref={ref}
+    className={
+      "rounded-md border-2 border-black px-2 py-2 text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-black"
+    }
+  />
+));
 
 export default Input;

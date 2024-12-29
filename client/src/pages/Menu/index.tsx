281e877c4f3387f8f47bcc23aceedb87e6fcb9fc
Franciszek Kaczmarek <franek.kaczmarek45gmail.com>
2024-12-30 00:21:13 +0100

    minor navigation fix

--- client/src/pages/Menu/index.tsx
+++ client/src/pages/Menu/index.tsx
@@ -6,40 +6,26 @@ function Menu() {
   const navigate = useNavigate();
 
   const [code, setCode] = useState("");
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setCode(e.target.value);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setCode(e.target.value);
 
   const handleJoinGame = useCallback(() => {
-    navigate(`/game/${code}`);
+    navigate(`/room/${code}`);
   }, [code, navigate]);
 
   return (
     <div className="m-auto mx-4 flex h-screen flex-col items-center justify-center gap-32">
       <div>
-        <h1 className="text-center text-6xl font-bold text-gray-900">
-          H1 Headline
-        </h1>
+        <h1 className="text-center text-6xl font-bold text-gray-900">H1 Headline</h1>
 
-        <p className="mt-2 text-center text-gray-400">
-          Lorem ipsum dolor sit amet.
-        </p>
+        <p className="mt-2 text-center text-gray-400">Lorem ipsum dolor sit amet.</p>
       </div>
 
       <div className="w-full sm:max-w-96">
         <label className="flex flex-col text-gray-900">
           Enter code
-          <Input
-            value={code}
-            onChange={handleChange}
-            maxLength={6}
-            type="text"
-          />
+          <Input value={code} onChange={handleChange} maxLength={6} type="text" />
         </label>
-        <Button
-          onClick={handleJoinGame}
-          disabled={code.length < 6}
-          className="mt-4 w-full disabled:opacity-60"
-        >
+        <Button onClick={handleJoinGame} disabled={code.length < 6} className="mt-4 w-full disabled:opacity-60">
           Join Game
         </Button>
         <div className="my-8 flex items-center justify-center">

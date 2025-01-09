fd5b4d1194ab4a9150b606b347be47ceb4183a9e
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-01-09 22:36:23 +0100

    refactor: store server url on client in .env

--- client/src/constants/socket.ts
+++ client/src/constants/socket.ts
@@ -1,6 +1,6 @@
 import type { CustomSocket } from "@/types/Socket";
 import { io } from "socket.io-client";
 
-export const socket: CustomSocket = io("ws://localhost:5000", {
+export const socket: CustomSocket = io(import.meta.env.VITE_SERVER_URL, {
   autoConnect: false,
 });

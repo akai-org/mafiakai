28bcd974068e7647e6d82ac6717f64d0ac1d10a9
MKaczor24 <mk.kaczor24@gmail.com>
2025-01-12 01:52:17 +0100

    Small UI improvements + simplified code

--- client/src/pages/Lobby/Waiting.tsx
+++ client/src/pages/Lobby/Waiting.tsx
@@ -1,51 +1,40 @@
 import { Button } from "@/components";
 
 function isReady(ready: boolean) {
-  if (ready) {
-    return <div className="mr-2 h-5 w-5 rounded-full bg-green-500"></div>;
-  } else {
-    return <div className="mr-2 h-5 w-5 rounded-full bg-red-500"></div>;
-  }
+  return <div className={`mr-2 h-5 w-5 rounded-full ${ready ? "bg-gradient-to-tr from-green-500 to-green-700" : "bg-gradient-to-tr from-red-500 to-red-700"}`}></div>;
 }
 
-function displayPlayers() {
+function DisplayPlayers(props: { players: string[] }) {
   return (
-    <>
-      <ul>
-        <li className="my-4 flex items-center text-lg">
-          {isReady(true)}
-          {"player name"}
-        </li>
-        <li className="my-4 flex items-center text-lg">
-          {isReady(false)}
-          {"player name"}
-        </li>
-        <li className="my-4 flex items-center text-lg">
-          {isReady(true)}
-          {"player name"}
-        </li>
-        <li className="my-4 flex items-center text-lg">
-          {isReady(true)}
-          {"player name"}
-        </li>
-        <li className="my-4 flex items-center text-lg">
-          {isReady(false)}
-          {"player name"}
-        </li>
-      </ul>
-    </>
+    <ul>
+        {/* Temporary loop from diversity in ready state */}
+      {props.players.map((name, i) => {
+        const ready = i % 2 === 0;
+        return (
+          <li className={`flex items-center my-4 text-lg ${ready ? "text-black" : "text-gray-500"}`}>{isReady(ready)}{name}</li>
+        );
+      })}
+    </ul>
   );
 }
 
-function Waiting() {
+function Waiting(props: { playername: string []}) {
+    {/*Temporary player name*/}
+    const playername = "Tomek"; 
   return (
     <div className="flex h-full w-full flex-col justify-between p-4">
-      <p className="text-justify">Be patient, the citizens are getting ready...</p>
-      <div className="flex flex-col gap-y-4">{displayPlayers()}</div>
-      <Button disabled>Ready</Button>
+      <p>Be patient {playername}, the citizens are getting ready...</p>
+      <div className="flex flex-col gap-y-4">
+        <DisplayPlayers players={["Tomek", "Kasia", "Marek", "Krzysztof", "Ania", "Michał", "Natalia"]}></DisplayPlayers>
+      </div>
+    <div className="relative group">
+        <Button size="button-lg" disabled className="w-full">Ready</Button>
+        <span className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded-md py-1 px-2">
+            Please create your character and choose a seat first
+        </span>
+    </div>
     </div>
   );
 }
 
 export default Waiting;
-

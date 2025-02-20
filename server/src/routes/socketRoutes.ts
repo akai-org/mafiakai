84c3703314bfaea716bbd875b89efc06e91b124e
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-02-20 22:01:52 +0100

    feat: assign players to roles

--- server/src/routes/socketRoutes.ts
+++ server/src/routes/socketRoutes.ts
@@ -20,10 +20,54 @@ function check_is_room_ready(room: Room): boolean {
   return players.length >= MINIMUM_NUMBER_OF_READY_PLAYERS_TO_START && !some_player_is_not_ready(players);
 }
 
+// Fisher-Yates Sorting Algorithm, pseudorandom only
+function shuffle(array: any[]): Array<any> {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const GUARANTEED_MAFIA_FOR_EACH_N_PLAYERS = 4;
+// For every 4 people add another mafia player
+// n mod 4 / 4 = chance to pick another mafia player
+// There is always 1 detective and 1 bodyguard
+function establish_roles(room: Room): boolean {
+  const players = room.getPlayers();
+  const n = players.length;
+  if (n < MINIMUM_NUMBER_OF_READY_PLAYERS_TO_START){
+    return false;
+  }
+
+  // Since after shuffling the array the order of players should be random enough
+  // the arbitrary indices can be modified to other numbers, but the ranges must be disjoint.
+  const players_shuffled = shuffle(players);
+  const detective = players_shuffled.at(0).id;
+  const bodyguard = players_shuffled.at(1).id;
+
+  const guaranteed_number_of_mafia = Math.floor(n/GUARANTEED_MAFIA_FOR_EACH_N_PLAYERS);
+  const chance_for_another_mafioso = (n % GUARANTEED_MAFIA_FOR_EACH_N_PLAYERS)/GUARANTEED_MAFIA_FOR_EACH_N_PLAYERS;
+  const another_mafioso = (Math.random() < chance_for_another_mafioso) ? 1 : 0;
+  const mafia_range_upper_bound_exclusive = 2+guaranteed_number_of_mafia+another_mafioso;
+  const mafia: Array<string> = players_shuffled.slice(2,mafia_range_upper_bound_exclusive).map((p: Player)=>{return p.id});
+
+  const regular_citizens = players_shuffled.slice(mafia_range_upper_bound_exclusive,n);
+
+  room.setPlayerRole(detective,Roles.DETECTIVE);
+  room.setPlayerRole(bodyguard,Roles.BODYGUARD);
+  for (const m of mafia){
+    room.setPlayerRole(m,Roles.MAFIOSO);
+  }
+  for (const c of regular_citizens){
+    room.setPlayerRole(c,Roles.REGULAR_CITIZEN);
+  }
+
+  return true;
+}
 
 // Here we set up the socket events for the client
 export default function socketRoutes(socket: MASocket) {
-  //TODO: Should player be assigned to socket room too?
   const room = manager.getRoom(socket.data.roomCode)!;
   const playerId = socket.data.playerId;
 
@@ -55,7 +99,5 @@ export default function socketRoutes(socket: MASocket) {
     callback(`OK: seated at ${position}`);
   });
 
-  socket.on("vote", (data: string) => {
-   
-  });
+  socket.on("vote", (data: string) => {});
 }

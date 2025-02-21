4dabe87789628154ff12f7b8ff2095b6be54fe10
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-02-21 15:49:12 +0100

    feat: add helper functions to Timer and Game

--- server/src/models/GameLogic.ts
+++ server/src/models/GameLogic.ts
@@ -1,14 +1,48 @@
 import { Phases } from "@global/Game";
-import { Room } from "./Room";
+import { Player, Room } from "./";
+import { Roles } from "@global/Roles";
+
+function sum(map: Map<any, number>): number {
+  var res = 0;
+  for (const val of map.values()) {
+    res += val;
+  }
+  return res;
+}
+
+function null_or_undefined(x: any | null | undefined) {
+  return x === null || x === undefined;
+}
 
 export class Timer {
   public isRunning = false;
+  private timeout_id: NodeJS.Timeout | null = null;
+  private callback: (() => void) | null = null;
+  private until: number | null = null;
   constructor(private time: number) {}
 
-  start(callback: () => void) {
-    if (this.isRunning) console.info("Timer is already running");
+  getUntil() {
+    return this.until;
+  }
+
+  extend(milliseconds: number) {
+    if (this.isRunning === false || null_or_undefined(this.timeout_id) || null_or_undefined(this.callback)) {
+      throw new Error("Nothing is scheduled.");
+    }
+    clearTimeout(this.timeout_id!);
     this.isRunning = true;
+    this.until = Date.now() + milliseconds;
     setTimeout(() => {
+      this.isRunning = false;
+      this.callback!();
+    }, milliseconds);
+  }
+
+  start(callback: () => void) {
+    this.isRunning = true;
+    this.until = Date.now() + this.time;
+    this.callback = callback;
+    this.timeout_id = setTimeout(() => {
       this.isRunning = false;
       callback();
     }, this.time);
@@ -18,7 +52,25 @@ export class Timer {
 export class Game {
   phase = new PhaseRouter(Phases.LOBBY);
   timer = new Timer(5000); // 5 seconds
+  common_vote = new Map<string, number>();
+  mafia_vote = new Map<string, number>();
+  chosen_by_detective: string = "";
+  chosen_by_bodyguard: string = "";
   constructor(public room: Room) {}
+
+  static find_winners(map: Map<string, number>) {
+    var max: number = 0;
+    var chosen: Array<string> = [];
+    for (const p of map) {
+      if (p[1] > max) {
+        max = p[1];
+        chosen = [p[0]];
+      } else if (p[1] === max) {
+        chosen.push(p[0]);
+      }
+    }
+    return chosen;
+  }
 }
 
 export type TransitionCondition = (game: Game) => Phases | null;

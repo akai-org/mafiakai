56f9a4c741a346b09c2a42ec539c2a44b35a201a
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-03-06 19:17:30 +0100

    add loggers and socket.io helpers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 import { MAServer } from "@/types";
 import { config } from "@/constants";
@@ -9,16 +10,33 @@ import { socketRoutes, webRouter } from "@/routes";
 
 const app = express(); // Create an Express application
 
+// if (process.env.NODE_ENV != "production" ){
+//   app.use((req, _, next) => {
+//     console.log(req);
+//     next();
+//   });
+// }
+
 // Create servers
 const httpServer = http.createServer(app); // Create an HTTP server
+
+const development_socketio_proxies = process.env.NODE_ENV != "production" ? ["https://admin.socket.io", "https://firecamp.dev"] : []
 
 // Create a WebSocket server
 const socketsServer: MAServer = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: ["http://localhost:5173",...development_socketio_proxies],
+    // methods: ["GET", "POST"],
+    credentials: true,
   },
 });
+
+if (process.env.NODE_ENV != "production" ){
+  instrument(socketsServer, {
+    auth: false,
+    mode: "development",
+  });
+  }
 
 // Configure servers
 socketsServer.use(socketAuth).on("connection", socketRoutes); // Configure the WebSocket server

9641852a428d8da0930b987bdb99fd94b2ee1e99
mit3r <franek.kaczmarek45gmail.com>
2025-03-14 14:06:03 +0100

    draft of interactions with game engine

--- server/src/engine/Game.ts
+++ server/src/engine/Game.ts
@@ -1,8 +1,9 @@
+import { Timer } from "./PhasesManager/Timer";
 import { Roles } from "@global/Roles";
-import { Timer } from "../models/Timer";
 import crypto from "node:crypto";
 import { PlayersManager } from "./PlayersManager/PlayersManager";
 import { PhasesManager } from "./PhasesManager/PhasesManager";
+import { Phases } from "@global/Game";
 
 export class Game {
   timer = new Timer(); // 5 seconds
@@ -12,39 +13,67 @@ export class Game {
   chosen_by_detective: string | null = null;
   chosen_by_bodyguard: string | null = null;
 
-  readonly socket_rooms = new Map<Roles, string>([
-    [Roles.REGULAR_CITIZEN, crypto.randomUUID()],
-    [Roles.MAFIOSO, crypto.randomUUID()],
-    [Roles.DETECTIVE, crypto.randomUUID()],
-    [Roles.BODYGUARD, crypto.randomUUID()],
-  ]);
+  // Functions to make actions in the game
+  proccessReady(playerId: string, ready: boolean) {
+    if (this.phase.current !== Phases.LOBBY) return; // TODO: throw new ClientError("gameAlreadyStarted");
+    const player = this.players.get(playerId);
+    if (!player) return; // TODO: throw new ClientError("playerNotFound");
 
-  private static find_winners(map: Map<string, number>) {
-    var max: number = 0;
-    var chosen: Array<string> = [];
-    for (const p of map) {
-      if (p[1] > max) {
-        max = p[1];
-        chosen = [p[0]];
-      } else if (p[1] === max) {
-        chosen.push(p[0]);
-      }
-    }
-    return chosen;
+    // TODO: ??? any other checks ???
+    player.isReady = ready;
   }
 
-  find_mafia_vote_winners() {
-    return Game.find_winners(this.mafia_vote);
+  proccessVote(playerId: string, target: string) {
+    const player = this.players.get(playerId);
+    if (!player) return; // TODO throw new ClientError("playerNotFound");
+    if (!player.role) return; // TODO throw new InternalError("playerHasNoRole");
+    if (!player.alive) return; // TODO throw new ClientError("playerIsDead");
+
+    const citizenPass = this.phase.current == Phases.VOTING && player.role !== Roles.MAFIOSO;
+    const mafiaPass = this.phase.current == Phases.VOTING && player.role === Roles.MAFIOSO;
+    if (!(citizenPass || mafiaPass)) return; // TODO throw new ClientError("youCannotVoteNow");
+
+    const targetPlayer = this.players.get(target);
+    if (!targetPlayer) return; // TODO throw new ClientError("targetNotFound");
+    if (!targetPlayer.alive) return; // TODO throw new ClientError("targetIsDead");
+
+    // TODO: ??? any other checks ???
+    player.vote = target;
   }
 
-  find_common_vote_winners() {
-    return Game.find_winners(this.common_vote);
-  }
+  // readonly socket_rooms = new Map<Roles, string>([
+  //   [Roles.REGULAR_CITIZEN, crypto.randomUUID()],
+  //   [Roles.MAFIOSO, crypto.randomUUID()],
+  //   [Roles.DETECTIVE, crypto.randomUUID()],
+  //   [Roles.BODYGUARD, crypto.randomUUID()],
+  // ]);
 
-  reset_votings() {
-    this.common_vote = new Map<string, number>();
-    this.mafia_vote = new Map<string, number>();
-    this.chosen_by_detective = "";
-    this.chosen_by_bodyguard = "";
-  }
+  // private static find_winners(map: Map<string, number>) {
+  //   var max: number = 0;
+  //   var chosen: Array<string> = [];
+  //   for (const p of map) {
+  //     if (p[1] > max) {
+  //       max = p[1];
+  //       chosen = [p[0]];
+  //     } else if (p[1] === max) {
+  //       chosen.push(p[0]);
+  //     }
+  //   }
+  //   return chosen;
+  // }
+
+  // find_mafia_vote_winners() {
+  //   return Game.find_winners(this.mafia_vote);
+  // }
+
+  // find_common_vote_winners() {
+  //   return Game.find_winners(this.common_vote);
+  // }
+
+  // reset_votings() {
+  //   this.common_vote = new Map<string, number>();
+  //   this.mafia_vote = new Map<string, number>();
+  //   this.chosen_by_detective = "";
+  //   this.chosen_by_bodyguard = "";
+  // }
 }

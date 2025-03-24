5f11c6be665d44e2e1d30d7831ddf894f9bab17a
mit3r <franek.kaczmarek45gmail.com>
2025-03-24 20:16:11 +0100

    fix: delete ended_at in timer

--- server/src/engine/PhasesManager/Timer.ts
+++ server/src/engine/PhasesManager/Timer.ts
@@ -3,10 +3,9 @@ import { InternalError } from "../InternalError";
 export class Timer {
   private isRunning = false;
 
-  get running(){
+  get running() {
     return this.isRunning;
   }
-
 
   private timeout_id: NodeJS.Timeout | null = null;
   private callback: (() => void) | null = null;
@@ -17,7 +16,6 @@ export class Timer {
     if (this.isRunning || milliseconds === 0) return;
     this.isRunning = true;
 
-
     this.callback = callback;
     this.timeout_id = setTimeout(() => {
       this.isRunning = false;
@@ -26,10 +24,7 @@ export class Timer {
   }
 
   extend(milliseconds: number) {
-    if (this.isRunning === false || !this.timeout_id || !this.callback)
-      throw new InternalError("cannotExtendTimer");
-
-    this.ended_at += milliseconds;
+    if (this.isRunning === false || !this.timeout_id || !this.callback) throw new InternalError("cannotExtendTimer");
 
     clearTimeout(this.timeout_id);
     this.timeout_id = setTimeout(() => {

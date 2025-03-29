e1cbe68828363d7e84117f98ffc745c73626b85b
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-03-29 11:38:46 +0100

    refactor: engine base tests

--- server/tests/engine_base.ts
+++ server/tests/engine_base.ts
@@ -1,16 +1,16 @@
 import Game from "@/engine/Game";
+import { PayloadError } from "@global/PayloadErrors";
 import { Phases } from "@global/Phases";
-import { ok, strictEqual } from "node:assert";
+import { ok, strictEqual, throws } from "node:assert";
 import { describe, it } from "node:test";
 
 describe("Game engine - basics", async () => {
-  let game: Game;
-
   await it("should be able to create a new game", () => {
-    game = new Game();
+    const game = new Game();
   });
 
   await it("should be able to add a player", () => {
+    const game = new Game();
     game._players.add("player1");
 
     strictEqual(game._players.all.length, 1);
@@ -24,6 +24,9 @@ describe("Game engine - basics", async () => {
   });
 
   await it("should be able to remove a player", () => {
+    const game = new Game();
+    game._players.add("player1");
+    game._players.add("player2");
     game._players.remove("player1");
 
     strictEqual(game._players.all.length, 1);
@@ -32,23 +35,69 @@ describe("Game engine - basics", async () => {
     ok(game._players.has("player2"));
   });
 
-  await it("should be able to mark a player as ready", () => {
-    game.ready("player2", true);
+  await it("should be able to assign a seat to a player", () => {
+    const game = new Game();
+    game._players.add("player2");
+    game.seatAt("player2",0);
+    ok(game._players.get("player2")?.seat == 0);
+  });
 
+  await it("should be able to assign a seat to multiple players", () => {
+    const game = new Game();
+    game._players.add("player1");
+    game.seatAt("player1",0);
+    game._players.add("player2");
+    game._players.add("player3");
+    game.seatAt("player3",1);
+    game.seatAt("player2",0);
+    ok(game._players.all);
+  });
+
+  await it("should be able to choose a persona", () => {
+    const game = new Game();
+    game._players.add("player2");
+    game.describePlayer("player2",{description: "Player 2"}) // TODO
+    ok(game._players.get("player2")?.persona );
+  });
+
+  await it("should be able to mark a player as ready if they have chosen a persona and a seat", () => {
+    const game = new Game();
+    game._players.add("player2");
+    game.seatAt("player2",0);
+    game.describePlayer("player2",{description: "Player 2"}) // TODO
+    game.ready("player2", true);
     ok(game._players.get("player2")?.isReady);
   });
 
-  await it("should be able to move to next phase if conditions are met", () => {
-    game.join("player1");
-    game.join("player3");
-    game.join("player4");
+  await it("should not be able to mark a player as ready otherwise than after a persona and a seat have been chosen", () => {
+    const game = new Game();
+    game._players.add("player2"); 
+    throws(
+      ()=>{
+        game.ready("player2", true);
+      },
+      new PayloadError("playerCannotBeReady")
+    );
+    ok(!game._players.get("player2")?.isReady);
+  });
 
+  await it("should be able to move to next phase if conditions are met", () => {
+    const game = new Game();
+
+    function join_to_ready(player: string){
+      game.join(player);
+      game.seatAt(player,0);
+      game.describePlayer(player,{description: player}) // TODO
+      game.ready(player, true);
+    }
+
+    join_to_ready("player2");
     strictEqual(game._phase.current, Phases.LOBBY);
-    game.ready("player1", true);
+    join_to_ready("player1");
     strictEqual(game._phase.current, Phases.LOBBY);
-    game.ready("player3", true);
+    join_to_ready("player3");
     strictEqual(game._phase.current, Phases.LOBBY);
-    game.ready("player4", true);
+    join_to_ready("player4");
     game.update();
     strictEqual(game._phase.current, Phases.ROLE_ASSIGNMENT);
   });

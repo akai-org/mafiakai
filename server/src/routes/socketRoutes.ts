5aed0755fd5aa399bf106917970211554e9984b2
Franciszek Kaczmarek <87528750+mit3r@users.noreply.github.com>
2025-04-07 17:05:52 +0200

    Update server/src/routes/socketRoutes.ts
    Co-authored-by: Tomek Pawłowski <t.pawlowski03@gmail.com>

--- server/src/routes/socketRoutes.ts
+++ server/src/routes/socketRoutes.ts
@@ -13,7 +13,7 @@ export default function socketRoutes(socket: MASocket) {
 
   // Bind game events to socket events
   socket.on("setReady", (readiness) => room.game.ready(socket.data.playerId, readiness));
-  socket.on("setSeat", (position: number) => room.game._players.setSeatFor(socket.data.playerId, position));
+  socket.on("setSeat", (position: number) => room.game._players.seatAt(socket.data.playerId, position));
   socket.on("setPlayerName", (name: string) => room.game._players.setNameFor(socket.data.playerId, name));
   socket.on("vote", (targetId: string) => room.game.vote(socket.data.playerId, targetId));
 
